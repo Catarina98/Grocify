287cce028c90f018a717d09060931263e6addaaf
Catarina98 <36730010+Catarina98@users.noreply.github.com>
2024-01-03 15:22:23 +0000

    plan config chosendays

--- GrocifyApp.DAL/Configurations/PlanConfig.cs
+++ GrocifyApp.DAL/Configurations/PlanConfig.cs
@@ -1,6 +1,7 @@
 ﻿using GrocifyApp.DAL.Models;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace GrocifyApp.DAL.Configurations
 {
@@ -9,6 +10,37 @@ namespace GrocifyApp.DAL.Configurations
         public void Configure(EntityTypeBuilder<Plan> builder)
         {
             builder.Property(b => b.Name).HasMaxLength(60);
+
+            builder.Property(x => x.ChoosenDays)
+             .HasColumnName(nameof(Plan.ChoosenDays))
+             .HasConversion(x => ConvertToDb(x), x => ConvertFromDb(x));
+
+        }
+
+        private static string? ConvertToDb(List<DaysOfWeek>? choosenDays)
+        {
+            return choosenDays == null ? null : String.Join(";", choosenDays);
+        }
+
+        private static List<DaysOfWeek>? ConvertFromDb(string? choosenDays)
+        {
+            if (choosenDays == null)
+            {
+                return null;
+            }
+
+            var daysArray = choosenDays.Split(';');
+            var result = new List<DaysOfWeek>();
+
+            foreach (var dayString in daysArray)
+            {
+                if (Enum.TryParse(dayString, out DaysOfWeek day))
+                {
+                    result.Add(day);
+                }
+            }
+
+            return result;
         }
     }
 }
--- GrocifyApp.DAL/Models/Plan.cs
+++ GrocifyApp.DAL/Models/Plan.cs
@@ -3,10 +3,21 @@
     public class Plan : BaseEntity
     {
         public required string Name { get; set; }
-        public required string ChoosenDays { get; set; }
+        public required List<DaysOfWeek>? ChoosenDays { get; set; }
         public bool MonthlyView { get; set; }
         public ICollection<PlanMealRecipe>? PlanMealRecipes { get; set; }
 
         //Todo: Add FK HouseId
+    }
+
+    public enum DaysOfWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
     }
 }
