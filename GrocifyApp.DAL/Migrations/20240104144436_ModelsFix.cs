ecefaad3b5bd6ea7040e13bf47b9e70856b8dacd
JessicaMira <144673379+JessicaMira@users.noreply.github.com>
2024-01-04 14:44:58 +0000

    Migration added

---
+++ GrocifyApp.DAL/Migrations/20240104144436_ModelsFix.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GrocifyApp.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModelsFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Plans");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Image",
+                table: "Recipes",
+                type: "varbinary(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Image",
+                table: "Recipes",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Plans",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
