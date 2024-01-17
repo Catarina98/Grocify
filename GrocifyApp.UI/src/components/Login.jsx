5d1191c3804b466c93181ec71f40f4f008248a65
Catarina98 <36730010+Catarina98@users.noreply.github.com>
2024-01-17 16:27:44 +0000

    login css

--- GrocifyApp.UI/src/components/Login.jsx
+++ GrocifyApp.UI/src/components/Login.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import ReactLogo from '../assets/logo.svg';
+import ReactLogo from '../assets/logo_with_text.svg';
+import './Login.jsx.scss';
 import LoginConsts from '../consts/enconsts/LoginConsts';
 import ApiEndpoints from '../consts/ApiEndpoints';
+import ArrowIcon from '../assets/arrow-ic.svg';
 
 
 const LoginForm = () => {
@@ -49,8 +51,8 @@ const LoginForm = () => {
         <div className="container-page">
             <img src={ReactLogo} alt="React Logo" />
             <form className="input-form">
-                <h2>{LoginConsts.SignIn}</h2>
-                <p>{LoginConsts.EnterDetails}</p>
+                <div className="title title--xl">{LoginConsts.SignIn}</div>
+                <p className="text color-n500 login-desc">{LoginConsts.EnterDetails}</p>
                 <div className="input-box mt-3">
                     <input className="input-text"
                         type="email" placeholder="name@example.com"
@@ -68,15 +70,21 @@ const LoginForm = () => {
                     <label className="input-placeholder">{LoginConsts.Password}</label>
                 </div>
 
-                <button type="button" className="primary-button" onClick={handleLogin}>
+                <button type="button" className="primary-button btn--xl" onClick={handleLogin}>
                     <span>{LoginConsts.SignIn}</span>
                     <div className="loading-button white"></div>
                 </button>
             </form>
 
-            <div className="text-center mt-4">
-                <div>{LoginConsts.HaveAccount}</div>
-                <a className="subtle-button" href="/register">{LoginConsts.SignUp}</a>
+            <div className="form-footer">
+                <div className="text color-n500">{LoginConsts.HaveAccount}</div>
+                <a className="subtle-button" href="/register">
+                    <span className="btn-text">{LoginConsts.SignUp}</span>
+
+                    <div className="btn-icon">
+                        <img className="arrow-right" src={ArrowIcon} alt="Sign up" />
+                    </div>
+                </a>
             </div>
         </div>
     );
