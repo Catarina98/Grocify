3273d32745206e245a02961060d749aba1472357
JessicaMira <144673379+JessicaMira@users.noreply.github.com>
2024-01-23 13:54:27 +0000

    fixing

--- GrocifyApp.UI/src/components/Layout/BottomNavbar.jsx
+++ GrocifyApp.UI/src/components/Layout/BottomNavbar.jsx
@@ -11,16 +11,16 @@ import SettingsIcon from '../../assets/settings-ic.svg';
 import AppRoutes from '../../consts/AppRoutes';
 
 const menuItems = [
-    { iconSrc: CartIcon, text: NavbarConsts.Lists },
-    { iconSrc: InventoryIcon, text: NavbarConsts.Inventories },
+    { iconSrc: CartIcon, text: NavbarConsts.Lists, route: '/weatherforecast' },
+    { iconSrc: InventoryIcon, text: NavbarConsts.Inventories, route: '/weatherforecast' },
     { iconSrc: RecipeIcon, text: NavbarConsts.Recipes },
-    { iconSrc: CalendarIcon, text: NavbarConsts.Plan },
-    { iconSrc: SettingsIcon, text: NavbarConsts.Settings },
+    { iconSrc: CalendarIcon, text: NavbarConsts.Plan, route: '/weatherforecast' },
+    { iconSrc: SettingsIcon, text: NavbarConsts.Settings, route: AppRoutes.Settings },
 ];
 
-const NavbarMenu = ({ iconSrc, text, activeNavItem, onClick }) => (
+const NavbarMenu = ({ iconSrc, text, route, activeNavItem, onClick }) => (
     <div className={`navbar-menu ${activeNavItem === text ? 'active' : ''}`}
-        onClick={() => onClick(text)}>
+        onClick={() => onClick(text, route)}>
         <div className="navbar-menu-icon">
             <ReactSVG className="react-svg" src={iconSrc} />
         </div>
@@ -31,6 +31,7 @@ const NavbarMenu = ({ iconSrc, text, activeNavItem, onClick }) => (
 NavbarMenu.propTypes = {
     iconSrc: PropTypes.string.isRequired,
     text: PropTypes.string.isRequired,
+    route: PropTypes.string,
     activeNavItem: PropTypes.string.isRequired,
     onClick: PropTypes.func.isRequired,
 };
@@ -39,10 +40,9 @@ const BottomNavbar = () => {
     const [activeNavItem, setActiveNavItem] = useState(NavbarConsts.Lists);
     const navigate = useNavigate();
 
-    const handleNavItemClick = (itemName) => {
-        setActiveNavItem(itemName);
-
-        navigate(AppRoutes.Settings);
+    const handleNavItemClick = (text, route) => {        
+        navigate(route);
+        setActiveNavItem(text);
     };
 
     return (
@@ -50,7 +50,9 @@ const BottomNavbar = () => {
             {menuItems.map((menuItem, index) => (
                 <NavbarMenu
                     key={index}
-                    {...menuItem}
+                    iconSrc={menuItem.iconSrc}
+                    text={menuItem.text}
+                    route={menuItem.route}
                     activeNavItem={activeNavItem}
                     onClick={handleNavItemClick}
                 />
--- GrocifyApp.UI/src/pages/Settings.jsx
+++ GrocifyApp.UI/src/pages/Settings.jsx
@@ -9,6 +9,7 @@ import LogoutIcon from '../assets/logout-ic.svg';
 import TrashIcon from '../assets/trash-ic.svg';
 import { SettingsConsts } from '../consts/ENConsts';
 import './Settings.jsx.scss';
+import Layout from '../components/Layout/Layout';
 
 const settingsItems = [
     {
@@ -39,102 +40,102 @@ function Settings() {
     const [darkMode, setDarkMode] = useState(false); //change to darkMode not false
 
     return (
-        <div className="container-settings">
-            <div className="searchbar-container">
-                <div className="searchbar-holder">
-                    {/*<CustomInputText @ref="SearchInputRef" Placeholder="@GenericsConst.Search"*/}
-                    {/*                 @bind-Value="SearchInput" class="app-form mb-0" Icon="@IconsCore.Generic.Search"*/}
-                    {/*                 OnInput="async x => await OnSearchChange(x)"/>*/}
-                    <CustomInput className="app-form mb-0"
-                        type="password"
-                        placeholder={PlaceholderConsts.Search}
-                        label={PlaceholderConsts.Search}
-                        value={searchInput}
-                        icon={SearchIcon}
-                        onChange={(e) => setSearchInput(e.target.value)} />
+        <Layout>
+            <div className="container-settings">
+                <div className="searchbar-container">
+                    <div className="searchbar-holder">
+                        {/*<CustomInputText @ref="SearchInputRef" Placeholder="@GenericsConst.Search"*/}
+                        {/*                 @bind-Value="SearchInput" class="app-form mb-0" Icon="@IconsCore.Generic.Search"*/}
+                        {/*                 OnInput="async x => await OnSearchChange(x)"/>*/}
+                        <CustomInput className="app-form mb-0"
+                            type="password"
+                            placeholder={PlaceholderConsts.Search}
+                            label={PlaceholderConsts.Search}
+                            value={searchInput}
+                            icon={SearchIcon}
+                            onChange={(e) => setSearchInput(e.target.value)} />
 
-                    {/*<div className="dropdown-search">*/}
-                    {/*    <div className="tab-buttons just-center">*/}
-                    {/*        */}{/*<TabSet TabsList="Tabs" SelectedTab="@SelectedTab"*/}
-                    {/*        */}{/*        SelectedTabChanged="OnSelectedTabChanged"/>*/}
-                    {/*    </div>*/}
-                    {/*    <div className="items-list">*/}
-                    {/*        */}{/*@if (FilteredDataList is null || !FilteredDataList.Any())*/}
-                    {/*        */}{/*{*/}
-                    {/*        */}{/*    <div class="no-results text--xs">*/}
-                    {/*        */}{/*        <svg data-src="@IconsConst.MainPage.SadSmile" class="icon-color--n600"/>*/}
+                        {/*<div className="dropdown-search">*/}
+                        {/*    <div className="tab-buttons just-center">*/}
+                        {/*        */}{/*<TabSet TabsList="Tabs" SelectedTab="@SelectedTab"*/}
+                        {/*        */}{/*        SelectedTabChanged="OnSelectedTabChanged"/>*/}
+                        {/*    </div>*/}
+                        {/*    <div className="items-list">*/}
+                        {/*        */}{/*@if (FilteredDataList is null || !FilteredDataList.Any())*/}
+                        {/*        */}{/*{*/}
+                        {/*        */}{/*    <div class="no-results text--xs">*/}
+                        {/*        */}{/*        <svg data-src="@IconsConst.MainPage.SadSmile" class="icon-color--n600"/>*/}
 
-                    {/*        */}{/*        @GenericsConst.NoResults*/}
-                    {/*        */}{/*    </div>*/}
-                    {/*        */}{/*}*/}
-                    {/*        */}{/*else*/}
-                    {/*        */}{/*{*/}
-                    {/*        */}{/*    @foreach (var item in FilteredDataList)*/}
-                    {/*        */}{/*    {*/}
-                    {/*        */}{/*        <button class="btn item-searched" @onclick="async () => await OnItemSelect(item)">*/}
-                    {/*        */}{/*            <div class="item-tab">*/}
-                    {/*        */}{/*                @if (item.Type == GlobalSearchType.Files)*/}
-                    {/*        */}{/*                {*/}
-                    {/*        */}{/*                    <div class="file-type @FilesHelper.GetFileExtensionColor(item.Icon)">*/}
-                    {/*        */}{/*                        @item.Icon*/}
-                    {/*        */}{/*                    </div>*/}
-                    {/*        */}{/*                }*/}
-                    {/*        */}{/*                else*/}
-                    {/*        */}{/*                {*/}
-                    {/*        */}{/*                    <svg data-src="@item.Icon" class="icon-color--n500"/>*/}
-                    {/*        */}{/*                }*/}
+                        {/*        */}{/*        @GenericsConst.NoResults*/}
+                        {/*        */}{/*    </div>*/}
+                        {/*        */}{/*}*/}
+                        {/*        */}{/*else*/}
+                        {/*        */}{/*{*/}
+                        {/*        */}{/*    @foreach (var item in FilteredDataList)*/}
+                        {/*        */}{/*    {*/}
+                        {/*        */}{/*        <button class="btn item-searched" @onclick="async () => await OnItemSelect(item)">*/}
+                        {/*        */}{/*            <div class="item-tab">*/}
+                        {/*        */}{/*                @if (item.Type == GlobalSearchType.Files)*/}
+                        {/*        */}{/*                {*/}
+                        {/*        */}{/*                    <div class="file-type @FilesHelper.GetFileExtensionColor(item.Icon)">*/}
+                        {/*        */}{/*                        @item.Icon*/}
+                        {/*        */}{/*                    </div>*/}
+                        {/*        */}{/*                }*/}
+                        {/*        */}{/*                else*/}
+                        {/*        */}{/*                {*/}
+                        {/*        */}{/*                    <svg data-src="@item.Icon" class="icon-color--n500"/>*/}
+                        {/*        */}{/*                }*/}
 
-                    {/*        */}{/*                <div class="text--xs">*/}
-                    {/*        */}{/*                    @item.Name*/}
-                    {/*        */}{/*                </div>*/}
-                    {/*        */}{/*            </div>*/}
+                        {/*        */}{/*                <div class="text--xs">*/}
+                        {/*        */}{/*                    @item.Name*/}
+                        {/*        */}{/*                </div>*/}
+                        {/*        */}{/*            </div>*/}
 
-                    {/*        */}{/*            @if (item.Type == GlobalSearchType.Topics)*/}
-                    {/*        */}{/*            {*/}
-                    {/*        */}{/*                <FollowBtn TopicId="(int) item.Id" TopicTitle="@item.Name" IsTopicPrivate="(bool) item.Value!"/>*/}
-                    {/*        */}{/*            }*/}
-                    {/*        */}{/*        </button>*/}
-                    {/*        */}{/*    }*/}
-                    {/*        */}{/*}*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
+                        {/*        */}{/*            @if (item.Type == GlobalSearchType.Topics)*/}
+                        {/*        */}{/*            {*/}
+                        {/*        */}{/*                <FollowBtn TopicId="(int) item.Id" TopicTitle="@item.Name" IsTopicPrivate="(bool) item.Value!"/>*/}
+                        {/*        */}{/*            }*/}
+                        {/*        */}{/*        </button>*/}
+                        {/*        */}{/*    }*/}
+                        {/*        */}{/*}*/}
+                        {/*    </div>*/}
+                        {/*</div>*/}
+                    </div>
                 </div>
-            </div>
 
-            <div className="container-cards">
-                {settingsItems.map(item => (
-                    <div className="card" key={item.tableName}>
-                        <div className="card-header title--s weight--l color-n600">{item.tableName}</div>
+                <div className="container-cards">
+                    {settingsItems.map(item => (
+                        <div className="card" key={item.tableName}>
+                            <div className="card-header title--s weight--l color-n600">{item.tableName}</div>
 
-                        <div className="card-body">
-                            {item.titles.map(title => (
-                                <div className="card-body-row" key={title}>
-                                    <div className="text">{title}</div>
+                            <div className="card-body">
+                                {item.titles.map(title => (
+                                    <div className="card-body-row" key={title}>
+                                        <div className="text">{title}</div>
 
 
-                                    {item.tableName === SettingsConsts.Appearance ? (
-                                        <div className="private-toggle">
-                                            <label className="toggle">
+                                        {item.tableName === SettingsConsts.Appearance ? (
+                                            <label className="toggle cursor-pointer">
                                                 <input type="checkbox" checked={darkMode} onChange={() => setDarkMode(!darkMode)} />
                                                 <span className="slider"></span>
                                             </label>
-                                        </div>
-                                    ) : (
-                                        <div className="icon--w16">
-                                            {item.tableName === SettingsConsts.Account ? (
-                                                <ReactSVG className={`react-svg ${title === SettingsConsts.ClearData ? 'color-r300' : ''}`} src={title === SettingsConsts.Logout ? LogoutIcon : TrashIcon} />
-                                            ) : (
-                                                <ReactSVG className="react-svg" src={title === SettingsConsts.DarkMode ? LogoutIcon : ChevronIcon} />
-                                            )}
-                                        </div>
-                                    )}
-                                </div>
-                            ))}
+                                        ) : (
+                                            <div className="icon--w16 cursor-pointer">
+                                                {item.tableName === SettingsConsts.Account ? (
+                                                    <ReactSVG className={`react-svg ${title === SettingsConsts.ClearData ? 'color-r300' : ''}`} src={title === SettingsConsts.Logout ? LogoutIcon : TrashIcon} />
+                                                ) : (
+                                                    <ReactSVG className="react-svg" src={title === SettingsConsts.DarkMode ? LogoutIcon : ChevronIcon} />
+                                                )}
+                                            </div>
+                                        )}
+                                    </div>
+                                ))}
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
-        </div>
+        </Layout>
     );
 }
 
