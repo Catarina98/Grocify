b0532e8e959143a7b7f13d22506954e617e54850
Catarina98 <36730010+Catarina98@users.noreply.github.com>
2024-01-23 15:41:33 +0000

    fixes

--- GrocifyApp.API/Models/ResponseModels/ShoppingListProductResponseModel.cs
+++ GrocifyApp.API/Models/ResponseModels/ShoppingListProductResponseModel.cs
@@ -2,13 +2,14 @@
 {
     public class ShoppingListProductResponseModel
     {
-        /// <example>Id</example>
+        /// <example>c7872109-cfd1-4332-b7f2-76c189987ae2</example>
         public required Guid Id { get; set; }
 
         /// <example>Quantity</example>
         public required int Quantity { get; set; }
 
-        /// <example>ProductId</example>
+        /// <example>c7872109-cfd1-4332-b7f2-76c189987ae2</example>
         public required Guid ProductId { get; set; }
+        public ProductResponseModel? Product { get; set; }
     }
 }
