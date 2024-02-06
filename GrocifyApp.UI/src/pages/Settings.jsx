826c9d50aaeffa0ca2cb984efa06aed0fef12e19
JessicaMira <144673379+JessicaMira@users.noreply.github.com>
2024-02-06 17:44:57 +0000

    fixed

--- GrocifyApp.UI/src/pages/Settings.jsx
+++ GrocifyApp.UI/src/pages/Settings.jsx
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types';
 //Internal components
 import Layout from '../components/Layout/Layout';
 import CustomInput from '../components/CustomInput';
-import BaseModal from '../components/BaseModal';
+import DefaultList from '../components/DefaultList';
 
 //Assets & Css
 import SearchIcon from '../assets/search-ic.svg';
@@ -23,8 +23,6 @@ import ApiEndpoints from '../consts/ApiEndpoints';
 function Settings(props) {  
     const token = localStorage.getItem('token');
     const [searchInput, setSearchInput] = useState('');
-    const [shoppingListData, setShoppingListData] = useState(null);
-    const [defaultShoppingList, setDefaultShoppingList] = useState(null);
     const [isModalOpen, setIsModalOpen] = useState(false);
     const navigate = useNavigate();    
 
@@ -50,54 +48,6 @@ function Settings(props) {
         }
     };
 
-    const getShoppingList = async () => {
-        try {
-            const response = await fetch(ApiEndpoints.ShoppingList_Endpoint, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`,
-                },
-            });
-
-            if (response.ok) {
-                const fetchedData = await response.json();
-                setShoppingListData(fetchedData);
-                setDefaultShoppingList(fetchedData.find(item => item.defaultList));
-            } else {
-                const errorData = await response.json();
-                console.log(errorData.errors[0]);
-            }
-        } catch (error) {
-            console.log(GenericConsts.Error);
-        }
-    };
-    
-    const updateDefaultShoppingList = async () => {
-        try {
-            if (token == undefined) {
-                return;
-            }
-
-            const response = await fetch(ApiEndpoints.DefaultShoppingList_Endpoint(defaultShoppingList.id), {
-                method: "PUT",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": `Bearer ${token}`
-                }
-            });
-
-            if (response.ok) {
-                console.log("updated");
-            } else {
-                const errorData = await response.json();
-                console.log(errorData.errors[0]);
-            }
-        } catch (error) {
-            console.log(GenericConsts.Error);
-        }
-    };
-
     const sendDataToParent = () => {                
         updateUserDarkMode();
         props.onDarkModeChange(!props.isDarkMode);
@@ -107,7 +57,7 @@ function Settings(props) {
 
     const openModal = () => {
         setIsModalOpen(true);
-        getShoppingList();
+        //getShoppingList();
     };
 
     const closeModal = () => {
@@ -195,18 +145,7 @@ function Settings(props) {
                     </div>
                 </div>
 
-                <BaseModal isOpen={isModalOpen} onClose={closeModal} buttonConfirm={updateDefaultShoppingList}>
-                    <div className={styles.contentList}>
-                        {shoppingListData != null && shoppingListData.length > 0 ? (
-                            shoppingListData.map((shoppingList) => (
-                                <div key={shoppingList.id} onClick={() => setDefaultShoppingList(shoppingList)}
-                                    className={`cursor-pointer text list-row + ${styles.listRow} ${shoppingList.id == defaultShoppingList.id ? styles.default + " default" : ''}`}>{shoppingList.name}</div>
-                            ))
-                        ) : (
-                            <div>No shopping list data available</div> //todo later (empty state)
-                        )}
-                    </div>
-                </BaseModal>
+                <DefaultList isOpen={isModalOpen} onClose={closeModal}/>
 
                 <div className={styles.containerCards}>
                     {settingsItems.map(settingTable => (
