93a47c9bc626cbc14129d9c350ee05c1bc1416e0
JessicaMira <144673379+JessicaMira@users.noreply.github.com>
2024-02-06 10:23:09 +0000

    fixed

--- GrocifyApp.DAL/Data/Consts/ENConsts/GenericConsts.cs
+++ GrocifyApp.DAL/Data/Consts/ENConsts/GenericConsts.cs
@@ -34,18 +34,18 @@
 
         public class ProductSections
         {
-            public ProductSectionModel Home = new(ProductSectionNames.Home, IconsConsts.ProductSectionIcons.Home);
-            public ProductSectionModel Fishmonger = new(ProductSectionNames.Fishmonger, IconsConsts.ProductSectionIcons.Fishmonger);
-            public ProductSectionModel Meat = new(ProductSectionNames.Meat, IconsConsts.ProductSectionIcons.Meat);
-            public ProductSectionModel Sweetgrocery = new(ProductSectionNames.Sweetgrocery, IconsConsts.ProductSectionIcons.Sweetgrocery);
-            //public ProductSectionModel Saltygrocery = new(ProductSectionNames.Saltygrocery, IconsConsts.ProductSectionIcons.Saltygrocery);
-            //public ProductSectionModel FrozenFood = new(ProductSectionNames.FrozenFood, IconsConsts.ProductSectionIcons.FrozenFood);
-            //public ProductSectionModel PersonalCare = new(ProductSectionNames.PersonalCare, IconsConsts.ProductSectionIcons.PersonalCare);
-            //public ProductSectionModel Bakery = new(ProductSectionNames.Bakery, IconsConsts.ProductSectionIcons.Bakery);
-            //public ProductSectionModel Dairy = new(ProductSectionNames.Dairy, IconsConsts.ProductSectionIcons.Dairy);
-            public ProductSectionModel FruitsVegetables = new(ProductSectionNames.FruitsVegetables, IconsConsts.ProductSectionIcons.FruitsVegetables);
-            //public ProductSectionModel Drinks = new(ProductSectionNames.Drinks, IconsConsts.ProductSectionIcons.Drinks);
-            //public ProductSectionModel Takeaway = new(ProductSectionNames.Takeaway, IconsConsts.ProductSectionIcons.Takeaway);
+            public ProductSectionModel Home = new(new Guid("5dcbf088-2b6e-4d03-845d-8564f2d6b19c"), ProductSectionNames.Home, IconsConsts.ProductSectionIcons.Home);
+            public ProductSectionModel Fishmonger = new(new Guid("38f03e29-8b04-4df3-a58b-7a8b5e912a95"), ProductSectionNames.Fishmonger, IconsConsts.ProductSectionIcons.Fishmonger);
+            public ProductSectionModel Meat = new(new Guid("72c6b5e5-9c7a-4345-9eb5-6f3a402ef4fd"), ProductSectionNames.Meat, IconsConsts.ProductSectionIcons.Meat);
+            public ProductSectionModel Sweetgrocery = new(new Guid("e0d9c3c1-0d4b-4e08-9dd5-3f2a8059b865"), ProductSectionNames.Sweetgrocery, IconsConsts.ProductSectionIcons.Sweetgrocery);
+            public ProductSectionModel Saltygrocery = new(new Guid("9a7cf8d1-8b57-4a37-90d5-06e5b4a33a1f"), ProductSectionNames.Saltygrocery, IconsConsts.ProductSectionIcons.Saltygrocery);
+            public ProductSectionModel FrozenFood = new(new Guid("ef3705e2-38e6-4ec8-8a06-8e3802d10ec5"), ProductSectionNames.FrozenFood, IconsConsts.ProductSectionIcons.FrozenFood);
+            public ProductSectionModel PersonalCare = new(new Guid("76a0cbe1-6d25-40c9-9cf5-430cd7e69d6d"), ProductSectionNames.PersonalCare, IconsConsts.ProductSectionIcons.PersonalCare);
+            public ProductSectionModel Bakery = new(new Guid("c44b52e7-19a2-4e4a-8dcb-bbe9cc5bb2e5"), ProductSectionNames.Bakery, IconsConsts.ProductSectionIcons.Bakery);
+            public ProductSectionModel Dairy = new(new Guid("18b5b01e-3a0f-4c69-96bf-2ea6c453c865"), ProductSectionNames.Dairy, IconsConsts.ProductSectionIcons.Dairy);
+            public ProductSectionModel FruitsVegetables = new(new Guid("a2f1755b-28b0-4b4a-86a1-9fb8c10b4880"), ProductSectionNames.FruitsVegetables, IconsConsts.ProductSectionIcons.FruitsVegetables);
+            public ProductSectionModel Drinks = new(new Guid("8f3a95f1-6b96-43b7-9f62-d3f3db4f0bf1"), ProductSectionNames.Drinks, IconsConsts.ProductSectionIcons.Drinks);
+            public ProductSectionModel Takeaway = new(new Guid("a1e9b4cb-946e-4ec5-927d-576c92b5b8f9"), ProductSectionNames.Takeaway, IconsConsts.ProductSectionIcons.Takeaway);
         }
 
         public class ProductMeasureNames
@@ -58,48 +58,61 @@
 
         public class ProductMeasures
         {
-            public ProductMeasureModel Gram = new(ProductMeasureNames.Gram);
-            public ProductMeasureModel Milliliter = new(ProductMeasureNames.Milliliter);
-            public ProductMeasureModel Unit = new(ProductMeasureNames.Unit);
-            public ProductMeasureModel TableSpoon = new(ProductMeasureNames.TableSpoon);
+            public ProductMeasureModel Gram = new(new Guid("a58e4f12-3f85-4b1f-9b41-6f5967b3d4b1"), ProductMeasureNames.Gram);
+            public ProductMeasureModel Milliliter = new(new Guid("c9b2a381-23f1-4d9a-9e2a-8a0bca31c3a2"), ProductMeasureNames.Milliliter);
+            public ProductMeasureModel Unit = new(new Guid("d4c7a8c3-729e-4b3d-9247-562a3d8c1e25"), ProductMeasureNames.Unit);
+            public ProductMeasureModel TableSpoon = new(new Guid("f0e88aa9-6d2f-4c88-a3fc-7543647d7ae1"), ProductMeasureNames.TableSpoon);
         }
 
         public class Products
         {
-            public ProductModel Apple = new("Apple", ProductMeasureNames.Unit, ProductSectionNames.FruitsVegetables);
+            public ProductModel Apple = new(new Guid("bd50a227-79db-4a7e-899c-1b0581c5931a"), "Apple", 
+                new Guid("d4c7a8c3-729e-4b3d-9247-562a3d8c1e25"), new Guid("a2f1755b-28b0-4b4a-86a1-9fb8c10b4880"));
+            public ProductModel Sugar = new(new Guid("452349cd-7cf8-4e52-916f-3fda94eab413"), "Sugar", 
+               new Guid("a58e4f12-3f85-4b1f-9b41-6f5967b3d4b1"), new Guid("e0d9c3c1-0d4b-4e08-9dd5-3f2a8059b865"));
+            public ProductModel Milk = new(new Guid("37bf06c3-d670-4ea7-9d54-815157f653f7"), "Milk", 
+               new Guid("c9b2a381-23f1-4d9a-9e2a-8a0bca31c3a2"), new Guid("18b5b01e-3a0f-4c69-96bf-2ea6c453c865"));
+            public ProductModel Egg = new(new Guid("9fca6f9a-ae7b-4cb4-bba0-2d2b4f16e5c2"), "Egg", 
+               new Guid("c9b2a381-23f1-4d9a-9e2a-8a0bca31c3a2"), new Guid("18b5b01e-3a0f-4c69-96bf-2ea6c453c865"));
         }
 
         public class ProductModel
         {
+            public Guid Id { get; set; }
             public string Name { get; set; }
-            public string Measure { get; set; }
-            public string Section { get; set; }
+            public Guid MeasureId { get; set; }
+            public Guid SectionId { get; set; }
 
-            public ProductModel(string name, string measure, string section)
+            public ProductModel(Guid id, string name, Guid measureId, Guid sectionId)
             {
+                Id = id;
                 Name = name;
-                Measure = measure;
-                Section = section;
+                MeasureId = measureId;
+                SectionId = sectionId;
             }
         }
 
         public class ProductMeasureModel
         {
+            public Guid Id { get; set; }
             public string Name { get; set; }
 
-            public ProductMeasureModel(string name)
+            public ProductMeasureModel(Guid id, string name)
             {
+                Id = id;
                 Name = name;
             }
         }
 
         public class ProductSectionModel
         {
+            public Guid Id { get; set; }
             public string Name { get; set; }
             public string Icon { get; set; }
 
-            public ProductSectionModel(string name, string icon)
+            public ProductSectionModel(Guid id, string name, string icon)
             {
+                Id = id;
                 Name = name;
                 Icon = icon;
             }
