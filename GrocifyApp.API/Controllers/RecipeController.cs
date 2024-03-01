b303ba63866bbb220854b0777f04c54261a41121
JessicaMira <144673379+JessicaMira@users.noreply.github.com>
2024-03-01 17:32:45 +0000

    fixing

---
+++ GrocifyApp.API/Controllers/RecipeController.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using GrocifyApp.API.Models.RequestModels;
+using GrocifyApp.API.Models.ResponseModels;
+using GrocifyApp.API.Services;
+using GrocifyApp.BLL.Interfaces;
+using GrocifyApp.DAL.Filters;
+using GrocifyApp.DAL.Models;
+
+namespace GrocifyApp.API.Controllers
+{
+    public class RecipeController : GenericControllerWithHouse<Recipe, RecipeRequestModel, RecipeResponseModel, BaseSearchModelWithHouse<Recipe>>
+    {
+        public RecipeController(IEntitiesServiceWithHouse<Recipe, BaseSearchModelWithHouse<Recipe>> recipeService, IMapper mapper, ICurrentUserService currentUserService)
+            : base(recipeService, mapper, currentUserService)
+        {
+        }
+
+        //todo: Remember on UI
+        // - first: we insert the details of the recipe (recipe controller)
+        // - second: we insert the ingredients of the recipe (recipeProduct)
+        // - third: we insert the directions of the recipe (direction controller) we need the RecipeId, so after everything filled, we need to insert
+        // the recipe, and after that, insert the direction the the recipeId returned from the insert method.
+    }
+}
---
+++ GrocifyApp.API/Models/RequestModels/DirectionRequestModel.cs
@@ -0,0 +1,14 @@
+﻿namespace GrocifyApp.API.Models.RequestModels
+{
+    public class DirectionRequestModel
+    {
+        /// <example>c7872109-cfd1-4332-b7f2-76c189987ae2</example>
+        public required Guid RecipeId { get; set; }
+
+        /// <example>Boil 500mL of water</example>
+        public required string Steps { get; set; }
+
+        /// <example>Filling</example>
+        public required string Name { get; set; }
+    }
+}
